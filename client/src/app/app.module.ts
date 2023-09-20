ddaeedc2e09fa2b9cffd101674e85b7713919d90
Martin Dominguez <martin.dominguez@supervielle.com.ar>
2023-09-20 19:55:33 -0300

    Fin de sección 6

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,9 +7,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { SharedModule } from './_modules/shared.module';
 
 
 @NgModule({
@@ -25,7 +25,7 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
