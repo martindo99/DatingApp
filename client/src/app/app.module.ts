bde595c600a4cfc38acf1c64638e301b7345597a
Martin Dominguez <martin.dominguez@supervielle.com.ar>
2023-12-04 20:23:15 -0300

    Fin de seccion 9

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,10 @@ import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
+
 
 
 @NgModule({
@@ -24,7 +28,9 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     RegisterComponent,
     TestErrorComponent,
     NotFoundComponent,
-    ServerErrorComponent
+    ServerErrorComponent,
+    MemberListComponent,
+    MemberCardComponent,
   ],
   imports: [
     BrowserModule,
@@ -35,7 +41,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     SharedModule
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
